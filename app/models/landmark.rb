7387674d39be221c954fc554a1bc284c293ae105
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-22 10:53:43 -0400

    added some notes during lecture

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -17,10 +17,16 @@ class Landmark
 
     #returns an **array** of all landmarks in that city
     def self.find_by_city(city)
-        self.all.select do |landmark|
+        Landmark.all.select do |landmark|
             landmark.city == city
         end
     end
+
+    # For both this one ^ and the similar one in tourist, we can use self in place
+    # of the class name (line 20) because this is a class method, and therefore is
+    # one level higher and refers to the class. @@all is bad convention even if it works
+    # because the self.all method that returns @@all could have some custom code in it,
+    # and could therefore not actually return all.
 
     #returns an **array** of all the trips taken to a given landmark
     def trips
--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -3,25 +3,40 @@ class Tourist
 
     attr_reader :name
 
+
     @@all = []
+
 
     def initialize(name)
         @name = name
         @@all << self
     end
 
+
+
     # should return **all** of the `Tourist` instances
     def self.all 
         @@all
     end
 
+
+
     # given a string of a name, returns the **first tourist** 
-    # whose name matches
+    # whose name matches. @@all is the same as Tourist.all
     def self.find_by_name(name)
-        @@all.find do |tourist|
+        Tourist.all.find do |tourist|
             tourist.name == name
         end
     end
+
+    # ^in this method, I was curious about whether we should be returning JUST the
+    # name, by setting line 21 to a variable and then returning variable.name. However,
+    # in general, we want to be able to interact with the entire OBJECT as much as 
+    # possible, since they are an actual **TOURIST** and not just a limited String,
+    # as a name would be. For example, if we used find_by_name later on, we wouldn't
+    # be able to do much with it because a string can't have methods tacked on to it.
+
+
 
     #returns an **array** of all the trips taken by the given Tourist
     def trips
@@ -30,12 +45,16 @@ class Tourist
         end
     end
 
+
+
     #returns an **array** of all the landmarks for the given Tourist
     def landmarks
         self.trips.collect do |trip|
             trip.landmark
         end
     end
+
+
 
     # Tourist#visit_landmark(landmark)` should create a new trip for 
     # that tourist to the given landmark
